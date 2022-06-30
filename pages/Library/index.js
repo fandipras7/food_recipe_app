5ee83c13db15898278974cbc0a8cb839164ae01f
fandipro <fprasetyo4@gmail.com>
2022-06-30 22:29:40 +0700

    tru deploy 1

--- pages/Library/index.js
+++ pages/Library/index.js
@@ -1,43 +1,43 @@
-import React from "react";
-import MyLayout from "../../component/layout/MyLayout";
-import Card from "../../component/base/Card";
-import axios from "axios";
-import { useRouter } from "next/router";
+// import React from "react";
+// import MyLayout from "../../component/layout/MyLayout";
+// import Card from "../../component/base/Card";
+// import axios from "axios";
+// import { useRouter } from "next/router";
 
-const Library = ({ recipes }) => {
-  const router = useRouter();
-  if (router.isFallback) {
-    return <h3>loading......</h3>;
-  }
-  return (
-    <div>
-      <MyLayout title="Library">
-        <div className="row">
-          {recipes.map((item) => (
-            <div key={item.id} className="col">
-              <Card
-                key={item.id}
-                img={item.image}
-                title={item.title}
-                onClick={() => {
-                  router.push(`/Library/${item.id}`);
-                }}
-              ></Card>
-            </div>
-          ))}
-        </div>
-      </MyLayout>
-    </div>
-  );
-};
+// const Library = ({ recipes }) => {
+//   const router = useRouter();
+//   if (router.isFallback) {
+//     return <h3>loading......</h3>;
+//   }
+//   return (
+//     <div>
+//       <MyLayout title="Library">
+//         <div className="row">
+//           {recipes.map((item) => (
+//             <div key={item.id} className="col">
+//               <Card
+//                 key={item.id}
+//                 img={item.image}
+//                 title={item.title}
+//                 onClick={() => {
+//                   router.push(`/Library/${item.id}`);
+//                 }}
+//               ></Card>
+//             </div>
+//           ))}
+//         </div>
+//       </MyLayout>
+//     </div>
+//   );
+// };
 
-export const getStaticProps = async () => {
-  const { data: RespData } = await axios.get("http://localhost:4000/v1/recipes");
-  return {
-    props: {
-      recipes: RespData.data,
-    },
-  };
-};
+// export const getStaticProps = async () => {
+//   const { data: RespData } = await axios.get("http://localhost:4000/v1/recipes");
+//   return {
+//     props: {
+//       recipes: RespData.data,
+//     },
+//   };
+// };
 
-export default Library;
+// export default Library;
